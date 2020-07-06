a298e0a1f11f17e0ad6d3de4e7faedf17edb8e9b
Clyde Griffin <deezy13@gmail.com>
2020-07-06 16:07:57 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,28 @@
 
-
+class Course
+  
+  
+  def initialize
+    
+  end 
+  
+  def title 
+    
+  end 
+  
+  def schedule
+    
+  end 
+  
+  def description
+    
+  end 
+  
+  def self.all 
+    
+  end 
+  
+  
+      
+  
+end 
