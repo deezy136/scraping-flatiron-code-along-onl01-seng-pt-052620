5d5b8211d4efbdcf4fe99c8a162e1bc1a91a8da3
Clyde Griffin <deezy13@gmail.com>
2020-07-06 16:13:48 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,25 +1,40 @@
 
 class Course
   
+  @@all = [] 
   
-  def initialize
-    
+  def initialize(title, schedule, description)
+    @title = title
+    @schedule = schedule
+    @description = description 
+    @@all << self 
   end 
   
+  def title=(title)
+    @title = title 
+  end 
+    
   def title 
     @title
   end 
   
-  def schedule
-    
+  def schedule=(schedule)
+    @schedule = schedule
   end 
   
+  def schedule
+    @schedule
+  end 
+  
+   def description
+    @description = description
+  end 
   def description
-    
+    @description
   end 
   
   def self.all 
-    
+    @@all 
   end 
   
   
